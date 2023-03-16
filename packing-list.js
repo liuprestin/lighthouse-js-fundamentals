d12c7e779962803d619a15137d0dfd07754fe2d3
Prestin Liu <liuprestin@gmail.com>
2023-03-15 22:29:55 -0700

    updating packing list example

--- packing-list.js
+++ packing-list.js
@@ -15,3 +15,10 @@ for (let i = 0; i < packingList.length; i++) {
 }
 
 packingList[3];
+
+console.log("Kitchen stuff to pack:");
+let i = 0;
+while (i < packingList.length) {
+  console.log(packingList[i]);
+  i++;
+}
