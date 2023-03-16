1e24c731262fa31c85d3d1336cceb2a482bcbf78
Prestin Liu <liuprestin@gmail.com>
2023-03-15 20:51:49 -0700

    added more functions with variables and a evenNess function

--- hello.js
+++ hello.js
@@ -1,13 +1,35 @@
 
 //assigning a function to a variable 
 const sayHello = function () {
-    console.log("Hello, world")
+    console.log("Hello, world");
 }
 
 sayHello();
 
 //more typical way to create functions 
-function HelloWorldAgain() {
-    console.log("Hello world again!")
+function HelloWorldAgain(num) {
+    console.log(`Hello world again! ${num}`);
 
 }
+
+HelloWorldAgain("not a number!!!");
+
+//using return 
+
+function sayHelloToConsole(name) {
+    return `Hello, ${name}`;
+}
+
+console.log(sayHelloToConsole("Bob"));
+
+// check evenness
+
+function isEven(num){
+    return num % === 0;
+}
+
+const tenIsEven = isEven(10);
+const elevenIsEven = isEven(11);
+
+console.log(tenIsEven);
+console.log(elevenIsEven);
